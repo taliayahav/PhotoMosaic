7d6976dd8e1e42b6c90e602768e0f99fdd6a63c5
Talia Yahav <taliayahav@Talias-MacBook-Air-2.local>
2022-01-09 20:33:19 -0500

    file method calculates RGB value of squares of image

--- src/main/java/yahav/photomosaic/PhotoMosaicApplication.java
+++ src/main/java/yahav/photomosaic/PhotoMosaicApplication.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 
 public class PhotoMosaicApplication extends Application {
 
@@ -28,10 +29,7 @@ public class PhotoMosaicApplication extends Application {
         int squares = 50;
         int xInc = width/squares;
         int yInc = height/squares;
-        //byte[] buffer = new byte[width * height * 4];
-        //PixelFormat<IntBuffer> pixelFormat = PixelFormat.getByteBgraInstance();
-        //int[] pixels = new int[Grid.SIZE]
-
+        Image[] croppedImages;
 
         // Create WritableImage
         WritableImage wImage = new WritableImage(
@@ -39,34 +37,50 @@ public class PhotoMosaicApplication extends Application {
                 (int) image.getHeight());
         PixelWriter pixelWriter = wImage.getPixelWriter();
 //        imageView.setImage(wImage);
-//        ArrayList<Rectangle2D> rectangles = new ArrayList<Rectangle2D>();
 
-        for(int x = 0; x < width; x+= xInc)
-            for(int y = 0; y < height; y+= yInc){
-                Rectangle2D rect = new Rectangle2D(x,y,xInc, yInc);
-//                rectangles.add(rect);
-                Color color = pixelReader.getColor(x,y);
-                System.out.print("section " + x + ", ");
-                System.out.println(y);
-                double red = ((color.getRed())/3);
-                double green = ((color.getGreen())/3);
-                double blue = ((color.getBlue())/3);
-//                System.out.println(avgColor);
-//                for (Rectangle2D rectangle :rectangles) {
-//                    System.out.println(color.getBlue()*255);
-//                }
-                color = Color.color(red, green, blue);
-                pixelWriter.setColor(x,y,color);
-            }
+//        ArrayList<Rectangle2D> rectangles = new ArrayList<Rectangle2D>();
+//        for(int x = 0; x < width; x+= xInc)
+//            for(int y = 0; y < height; y+= yInc){
+//                Rectangle2D rect = new Rectangle2D(x,y,xInc, yInc);
+////                rectangles.add(rect);
+//                Color color = pixelReader.getColor(x,y);
+//                System.out.print("section " + x + ", ");
+//                System.out.println(y);
+//                double red = ((color.getRed()*255));
+//                double green = ((color.getGreen()*255));
+//                double blue = ((color.getBlue()*255));
+////                System.out.println(avgColor);
+////                for (Rectangle2D rectangle :rectangles) {
+////                    System.out.println(color.getBlue()*255);
+////                }
+//                color = Color.color(red, green, blue);
+//                pixelWriter.setColor(x,y,color);
+//            }
+        //Should each square go into an arrayList?
+
         File[] file = new File("/Users/taliayahav/PhotoMosaic/src/main/resources/flower").listFiles();
+        croppedImages = new Image[file.length];
         for(File img : file){
             Image fileImage = new Image(img.toURI().toString());
             ImageView fileImageView = new ImageView();
             fileImageView.setImage(fileImage);
+            for(int x = 0; x < width; x+= xInc)
+                for(int y = 0; y < height; y+= yInc){
+                    Color srcImgClr = pixelReader.getColor(x,y);
+                    double red = ((srcImgClr.getRed()*255));
+                    double green = ((srcImgClr.getGreen()*255));
+                    double blue = ((srcImgClr.getBlue()*255));
+                    System.out.println("R: " + red);
+                    System.out.println("G:" + green);
+                    System.out.println("B: " + blue);
+                }
+//            for(int i= 0; i < croppedImages.length; i++){
+//                //Image fileName = img[i];
+//                //croppedImages[i] = fileName;
+//            }
         }
-
-
-
+        //How do I trim each photo?
+        //Should each photo also go into a list to swap with the other list?
 
         // Display image on screen
         imageView.setImage(wImage);
