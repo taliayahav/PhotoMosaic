fa16241448b7cb306e3928a7e1a34906cbb4459d
Talia Yahav <taliayahav@Talias-MacBook-Air-2.local>
2022-01-10 17:47:41 -0500

    method to obtain pixels of image

---
+++ src/main/java/yahav/photomosaic/ImagePixels.java
@@ -0,0 +1,32 @@
+package yahav.photomosaic;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ImagePixels{
+    Image image;
+    int width = (int)image.getWidth();
+    int height = (int)image.getHeight();
+    public ImagePixels() throws IOException {
+        FileInputStream input = new FileInputStream("src/main/resources/google.png");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView();
+        PixelReader pixelReader = image.getPixelReader();
+    }
+
+
+    public int[][] getImagePixels(Image image, int width, int height) {
+        PixelReader pixelReader = image.getPixelReader();
+        int[][] pixels = new int[width][height];
+        for(int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                pixels[row][col] = image.getPixelReader().getArgb(row,col);
+            }
+        }
+        return pixels;
+    }
+}
--- src/main/java/yahav/photomosaic/PhotoMosaicApplication.java
+++ src/main/java/yahav/photomosaic/PhotoMosaicApplication.java
@@ -24,12 +24,12 @@ public class PhotoMosaicApplication extends Application {
         FileInputStream input = new FileInputStream("src/main/resources/google.png");
         Image image = new Image(input);
         ImageView imageView = new ImageView();
-        imageView.setFitHeight(500);
-        imageView.setFitWidth(500);
+        imageView.setFitHeight(300);
+        imageView.setFitWidth(300);
         imageView.setImage(image);
 
 
-        PixelReader pixelReader = image.getPixelReader();
+        //PixelReader pixelReader = image.getPixelReader();
         int squares = 50;
         int width = (int)image.getWidth();
         int height = (int)image.getHeight();
@@ -40,61 +40,75 @@ public class PhotoMosaicApplication extends Application {
                 (int) image.getWidth(),
                 (int) image.getHeight());
         PixelWriter pixelWriter = wImage.getPixelWriter();
+        int[][] boxes = new int[width][height];
         double redPixels = 0;
         double greenPixels = 0;
         double bluePixels = 0;
         double numPixels = 0;
-        for(int x = 0; x < width; x+= xInc){
-            for(int y = 0; y < height; y+= yInc){
-                Color color = pixelReader.getColor(x,y);
-                System.out.print("section " + x + ", ");
-                System.out.println(y);
-                redPixels += (color.getRed());
-                greenPixels += (color.getGreen());
-                bluePixels += (color.getBlue());
-                numPixels++;
-                double red = (redPixels);
-                double green = (greenPixels);
-                double blue = (bluePixels);
-                color = Color.color(red/numPixels, green/numPixels, blue/numPixels);
-                pixelWriter.setColor(x,y,color);
-            }
-        }
-        Image[] croppedImages;
-        File[] file = new File("src/main/resources/flower").listFiles();
-        croppedImages = new Image[file.length];
+//        for(int squareX = 0; squareX < width; squareX+= xInc) {
+//            for (int squareY = 0; squareY < height; squareY += yInc) {
+//
+//            }
+//        }
+//            for(int x = 0; x < width; x++) {
+//                for(int y = 0; y <width; y++){
+//                    Color color = pixelReader.getColor(x, y);
+//                    redPixels += color.getRed();
+//                    greenPixels += color.getGreen();
+//                    bluePixels += color.getBlue();
+//                    numPixels++;
+//                    double red = (redPixels);
+//                    double green = (greenPixels);
+//                    double blue = (bluePixels);
+//                    color = Color.color(red / numPixels, green / numPixels, blue / numPixels);
+//                    pixelWriter.setColor(x, y, color);
+//                }
+//            }
+
+
+            //my code separates the image to squares but doesn't add what's in each individual
+            //need to add each pixel within each square
+
+            //x,y = boxes of an image
+            //return? so can be used for pixelwriter
+
+//        Image[] croppedImages;
+//        File[] file = new File("src/main/resources/flower").listFiles();
+//        croppedImages = new Image[file.length];
 //        for(File img : file)
-        for(int i=0; i< croppedImages.length; i++){
-            Image fileImage = new Image(file[i].toURI().toString()); //converts filename to image
-            ImageView fileImageView = new ImageView(); //set image in file to imageview
-            fileImageView.setImage(fileImage);
-            croppedImages[i] = fileImage; //adds each image to croppedImages list
-            System.out.println(fileImage);
-            int w = (int)croppedImages[i].getWidth();
-            int h = (int)croppedImages[i].getHeight();
-            int incImageX = w/squares;
-            int incImageY = h/squares;
-            for(int x = 0; x < w; x+= incImageX) {
-                for (int y = 0; y < h; y += incImageY) {
-                    Color color = pixelReader.getColor(x,y);
-                    System.out.print("section " + x + ", ");
-                    System.out.println(y);
-                    redPixels += (color.getRed());
-                    greenPixels += (color.getGreen());
-                    bluePixels += (color.getBlue());
-                    numPixels++;
-                    double red = (redPixels);
-                    double green = (greenPixels);
-                    double blue = (bluePixels);
-                    color = Color.color(red/numPixels, green/numPixels, blue/numPixels);
-            }
-        }
+//        for(int i=0; i< croppedImages.length; i++) {
+//            Image fileImage = new Image(file[i].toURI().toString()); //converts filename to image
+//            ImageView fileImageView = new ImageView(); //set image in file to imageview
+//            fileImageView.setImage(fileImage);
+//            croppedImages[i] = fileImage; //adds each image to croppedImages list
+//            System.out.println(fileImage);
+//            int w = (int) croppedImages[i].getWidth();
+//            int h = (int) croppedImages[i].getHeight();
+//            int incImageX = w / squares;
+//            int incImageY = h / squares;
+//            for (int x = 0; x < w; x += incImageX) {
+//                for (int y = 0; y < h; y += incImageY) {
+//                    Color color = pixelReader.getColor(x, y);
+//                    System.out.print("section " + x + ", ");
+//                    System.out.println(y);
+//                    redPixels += (color.getRed());
+//                    greenPixels += (color.getGreen());
+//                    bluePixels += (color.getBlue());
+//                    numPixels++;
+//                    double red = (redPixels);
+//                    double green = (greenPixels);
+//                    double blue = (bluePixels);
+//                    color = Color.color(red / numPixels, green / numPixels, blue / numPixels);
+//                }
+//            }
+//        }
+            //how to crop these images?
 
         // Display image on screen
         imageView.setImage(wImage);
         StackPane root = new StackPane();
         root.getChildren().add(imageView);
-        Scene scene = new Scene(root, 500, 500);
+        Scene scene = new Scene(root, 300, 300);
         primaryStage.setTitle("Image Read Test");
         primaryStage.setScene(scene);
         primaryStage.show();
