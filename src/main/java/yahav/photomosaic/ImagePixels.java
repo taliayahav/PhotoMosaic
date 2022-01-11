465cd0da699b7845b0c582622a6c7d8f41b1a881
Talia Yahav <taliayahav@Talias-MacBook-Air-2.local>
2022-01-10 20:03:03 -0500

    method returns average color of pixel of square

--- src/main/java/yahav/photomosaic/ImagePixels.java
+++ src/main/java/yahav/photomosaic/ImagePixels.java
@@ -4,6 +4,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelFormat;
 import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -17,17 +18,33 @@ public class ImagePixels{
         Image image = new Image(input);
         ImageView imageView = new ImageView();
         PixelReader pixelReader = image.getPixelReader();
+        int width = (int)image.getWidth();
+        int height = (int)image.getHeight();
     }
 
 
-    public int[] getImagePixels(Image image, int width, int height) {
+    public Color getImagePixels(Image image, int width, int height) {
+        int squares = 50;
+        int xInc = width/squares;
+        int yInc = height/squares;
+        double redPixels = 0;
+        double greenPixels = 0;
+        double bluePixels = 0;
+        double numPixels = 0;
         PixelReader pixelReader = image.getPixelReader();
-        int[] pixels = new int[width*height];
-        for(int row = 0; row < height; row++) {
-            for (int col = 0; col < width; col++) {
-                pixelReader.getPixels(row,col,width,height, PixelFormat.getIntArgbInstance(),pixels,0,width);
+        for(int squareX = 0; squareX < height; squareX+= xInc) {
+            for (int squareY = 0; squareX < width; squareX += yInc) {
+                for (int x = 0; x < squareX; x++) {
+                    for (int y = 0; y < squareY; y++) {
+                        Color color = pixelReader.getColor(squareX, squareY);
+                        redPixels += color.getRed();
+                        greenPixels += color.getGreen();
+                        bluePixels += color.getBlue();
+                        numPixels++;
+                    }
+                }
             }
         }
-        return pixels;
+        return Color.color(redPixels / numPixels, greenPixels / numPixels, bluePixels / numPixels);
     }
 }
--- src/main/java/yahav/photomosaic/PhotoMosaicApplication.java
+++ src/main/java/yahav/photomosaic/PhotoMosaicApplication.java
@@ -28,58 +28,13 @@ public class PhotoMosaicApplication extends Application {
         imageView.setFitHeight(300);
         imageView.setFitWidth(300);
         imageView.setImage(image);
-        PixelFormat<IntBuffer> pixelFormat = PixelFormat.getIntArgbInstance();
 
-
-
-        //PixelReader pixelReader = image.getPixelReader();
-        int squares = 50;
-        int width = (int)image.getWidth();
-        int height = (int)image.getHeight();
-        int xInc = height/squares;
-        int yInc =
-        width/squares;
         // Create WritableImage
         WritableImage wImage = new WritableImage(
                 (int) image.getWidth(),
                 (int) image.getHeight());
         PixelWriter pixelWriter = wImage.getPixelWriter();
         PixelReader pixelReader = image.getPixelReader();
-        int[][] boxes = new int[width][height];
-        int [] pixels = new int[width*height*3];
-        double redPixels = 0;
-        double greenPixels = 0;
-        double bluePixels = 0;
-        double numPixels = 0;
-//        for(int x = 0; x <)
-//        pixelReader.getPixels(squareX,squareY,xInc,yInc, (WritablePixelFormat<IntBuffer>) pixelFormat,pixels,0,width);
-        
-//        for(int squareX = 0; squareX < height; squareX+= xInc) {
-//            for (int squareY = 0; squareY < width; squareY += yInc) {
-//                pixelReader.getPixels(squareX,squareY,xInc,yInc, (WritablePixelFormat<IntBuffer>) pixelFormat,pixels,0,width*3);
-//            }
-//        }
-//            for(int x = 0; x < width; x++) {
-//                for(int y = 0; y <width; y++){
-//                    Color color = pixelReader.getColor(x, y);
-//                    redPixels += color.getRed();
-//                    greenPixels += color.getGreen();
-//                    bluePixels += color.getBlue();
-//                    numPixels++;
-//                    double red = (redPixels);
-//                    double green = (greenPixels);
-//                    double blue = (bluePixels);
-//                    color = Color.color(red / numPixels, green / numPixels, blue / numPixels);
-//                    pixelWriter.setColor(x, y, color);
-//                }
-//            }
-
-
-            //my code separates the image to squares but doesn't add what's in each individual
-            //need to add each pixel within each square
-
-            //x,y = boxes of an image
-            //return? so can be used for pixelwriter
 
 //        Image[] croppedImages;
 //        File[] file = new File("src/main/resources/flower").listFiles();
@@ -114,7 +69,7 @@ public class PhotoMosaicApplication extends Application {
             //how to crop these images?
 
         // Display image on screen
-        imageView.setImage(wImage);
+        imageView.setImage(image);
         StackPane root = new StackPane();
         root.getChildren().add(imageView);
         Scene scene = new Scene(root, 300, 300);
